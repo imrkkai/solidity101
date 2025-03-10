eb96088533342607c7d97929e378123564440b26
imrkkai <imrkk@proton.me>
2025-03-10 16:17:50 +0800

    note

--- Function.sol
+++ Function.sol
@@ -1,6 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.21;
 
+
+/**
+{internal|external|public|private}：函数可见性说明符，共有4种。
+public：内部和外部均可见。
+private：只能从本合约内部访问，继承的合约也不能使用。
+external：只能从合约外部访问（但内部可以通过 this.f() 来调用，f是函数名）。
+internal: 只能从合约内部访问，继承的合约可以用。
+**/
+
 contract FunctionTypes {
     uint256 public number = 5;
 
